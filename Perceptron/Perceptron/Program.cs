0efc9884a722b1c396d1a8eaf958ff23dfa8ceed
Kamil Mrowiec <mistrz.karacik@gmail.com>
2018-04-12 00:18:43 +0200

    Gotowe?

---
+++ Perceptron/Perceptron/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var perc = new Perceptron(3);
+            var inputs = new double[,]
+             {
+                {1, 2, 1},
+                {1, 2, 2},
+                {1, 0, 6},
+                {1, -2, 10},
+                {1, -2, 0},
+                {1, 0, 0},
+                {1, 4, -20}
+             };
+            var outputs = new double[]
+            {
+                1, 1, 1, -1, -1, -1, -1
+            };
+            Console.WriteLine($"\n eps:{perc.CalculateEpsilon(inputs,outputs)} ");
+            perc.Train(inputs, outputs, 0);
+            Console.WriteLine();
+            //perc.Test(inputs, outputs);
+            Console.ReadLine();
+
+        }
+    }
+}
